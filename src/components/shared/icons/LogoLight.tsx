5f79a1c319af4812f3cfa117a302bf6b15046c76
Sohaib <sohaib@macs-MacBook-Pro.local>
2024-02-26 13:19:42 +0500

    update logo

--- src/components/shared/icons/LogoLight.tsx
+++ src/components/shared/icons/LogoLight.tsx
@@ -3,7 +3,7 @@ import Image from "../Image";
 const LogoLight = ({ className }: ISVGProps) => {
   return (
      <Image
-      src='/public//img/logo-02.png'
+      src='/public//img/A-logo.png'
       alt='logo'
       height={218} width={218}
       className={className}
